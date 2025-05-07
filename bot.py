17c01357f6bc143c04a778d429299cd0b94fd985
mikhaildoroshenko <mixdorosh@gmail.com>
2025-05-07 12:44:23 +0700

    Отправка и получение изображений, видео и документов.

--- bot.py
+++ bot.py
@@ -23,5 +23,24 @@ def handler_text(message):
     response = f"Ваш текст: {message.text}"
     bot.send_message(message.chat.id, response)
 
+
+# Отправка и получение изображений, видео и документов.
+@bot.message_handler(content_types=['photo'])
+def handler_photo(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    with open("received_image.jpg", 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    bot.reply_to(message, "Изображение получено и сохранено")
+
+
+# отправка изображения в ответ на команду /sendphoto
+@bot.message_handler(commands=['sendphoto'])
+def send_photo(message):
+    photo = open('path_to_your_image.jpg', 'rb')
+    bot.send_photo(message.chat.id, photo)
+
 # Запуск бота
 bot.polling()
